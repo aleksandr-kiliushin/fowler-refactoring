2a1b6ea516473db265d45b4c50121a6243afbf6c
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-04-15 13:01:55 +0400

    ch1: Extract functions, put in the statement fn body and rename vars

--- src/statement/index.ts
+++ src/statement/index.ts
@@ -1,5 +1,3 @@
-import amountFor from "./amountFor"
-
 function statement(invoice: any, plays: any) {
   let totalAmount = 0
   let volumeCredits = 0
@@ -10,16 +8,16 @@ function statement(invoice: any, plays: any) {
     minimumFractionDigits: 2,
   }).format
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID]
-    let thisAmount = amountFor(perf, play)
+    let thisAmount = amountFor(perf)
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0)
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5)
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5)
 
     // print line for this order
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
+    result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     } seats)\n`
     totalAmount += thisAmount
@@ -27,6 +25,32 @@ function statement(invoice: any, plays: any) {
   result += `Amount owed is ${format(totalAmount / 100)}.\n`
   result += `You earned ${volumeCredits} credits.\n`
   return result
+
+  function amountFor(aPerformance: any): number {
+    let result = 0
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30)
+        }
+        break
+      case "comedy":
+        result = 30000
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20)
+        }
+        result += 300 * aPerformance.audience
+        break
+      default:
+        throw new Error(`Unknown type: ${playFor(aPerformance).type}.`)
+    }
+    return result
+  }
+
+  function playFor(aPerformance: any): any {
+    return plays[aPerformance.playID]
+  }
 }
 
 export default statement
