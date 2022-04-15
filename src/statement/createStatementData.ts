be2111ee1653d8899975f1ef712b25cb16bdb910
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-04-15 23:51:49 +0400

    ch1: Use polymorhpic calculator

--- src/statement/createStatementData.ts
+++ src/statement/createStatementData.ts
@@ -7,44 +7,16 @@ export default function createStatementData(invoice: any, plays: any) {
   return statementData
 
   function enrichPerformance(aPerformance: any): any {
+    const calculator = createPerformaceCalculator(aPerformance, playFor(aPerformance))
     const result = Object.assign({}, aPerformance)
-    result.play = playFor(result)
-    result.amount = amountFor(result)
-    result.volumeCredits = volumeCreditsFor(result)
+    result.play = calculator.play
+    result.amount = calculator.amount
+    result.volumeCredits = calculator.volumeCredits
     return result
   }
 
   function playFor(aPerformance: any): any {
     return plays[aPerformance.playID]
-  }
-
-  function amountFor(aPerformance: any): number {
-    let result = 0
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30)
-        }
-        break
-      case "comedy":
-        result = 30000
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20)
-        }
-        result += 300 * aPerformance.audience
-        break
-      default:
-        throw new Error(`Unknown type: ${aPerformance.play.type}.`)
-    }
-    return result
-  }
-
-  function volumeCreditsFor(aPerformance: any): number {
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0)
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5)
-    return result
   }
 
   function totalAmount(data: any): any {
@@ -53,5 +25,59 @@ export default function createStatementData(invoice: any, plays: any) {
 
   function totalVolumeCredits(data: any): any {
     return data.performances.reduce((total: any, p: any) => total + p.volumeCredits, 0)
+  }
+}
+
+class PerformanceCalculator {
+  public performance: any
+  public play: any
+  public get: any
+  constructor(aPerformance: any, aPlay: any) {
+    this.performance = aPerformance
+    this.play = aPlay
+  }
+
+  get amount(): number {
+    throw new Error(
+      "This method is supposed to be overriden in children class not supposed to be called."
+    )
+  }
+
+  get volumeCredits() {
+    return Math.max(this.performance.audience - 30, 0)
+  }
+}
+
+function createPerformaceCalculator(aPerformance: any, aPlay: any): any {
+  switch (aPlay.type) {
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay)
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay)
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount(): number {
+    let result = 30000
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20)
+    }
+    result += 300 * this.performance.audience
+    return result
+  }
+
+  get volumeCredits(): number {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5)
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30)
+    }
+    return result
   }
 }
