264893cb817faf944a84c20902d278a3cd42230b
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-04-15 10:59:01 +0400

    ch1: Rename: result and aPerformance

--- src/statement/amountFor.ts
+++ src/statement/amountFor.ts
@@ -1,23 +1,23 @@
-const amountFor = (perf: any, play: any): number => {
-  let thisAmount = 0
+const amountFor = (aPerformance: any, play: any): number => {
+  let result = 0
   switch (play.type) {
     case "tragedy":
-      thisAmount = 40000
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30)
+      result = 40000
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30)
       }
       break
     case "comedy":
-      thisAmount = 30000
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20)
+      result = 30000
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20)
       }
-      thisAmount += 300 * perf.audience
+      result += 300 * aPerformance.audience
       break
     default:
       throw new Error(`Unknown type: ${play.type}.`)
   }
-  return thisAmount
+  return result
 }
 
 export default amountFor
