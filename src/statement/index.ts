ec42445e799d08ada58ba39e65071883fe6d2f87
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-04-15 19:33:48 +0400

    ch1: put createStatementData to a separate module, replace loops with reduces

--- src/statement/index.ts
+++ src/statement/index.ts
@@ -1,51 +1,17 @@
-function statement(invoice: any, plays: any) {
-  const statementData: any = {}
-  statementData.customer = invoice.customer
-  statementData.performances = invoice.performances
-  return renderPlainText(statementData, plays)
+import createStatementData from "./createStatementData"
+
+function statement(invoice: any, plays: any): any {
+  return renderPlainText(createStatementData(invoice, plays))
 }
 
-function renderPlainText(data: any, plays: any) {
+function renderPlainText(data: any) {
   let result = `Statement for ${data.customer}\n`
   for (const perf of data.performances) {
-    result += `  ${playFor(perf).name}: ${formatAsUSD(amountFor(perf))} (${perf.audience} seats)\n`
+    result += `  ${perf.play.name}: ${formatAsUSD(perf.amount)} (${perf.audience} seats)\n`
   }
-  result += `Amount owed is ${formatAsUSD(totalAmount())}.\n`
-  result += `You earned ${totalVolumeCredits()} credits.\n`
+  result += `Amount owed is ${formatAsUSD(data.totalAmount)}.\n`
+  result += `You earned ${data.totalVolumeCredits} credits.\n`
   return result
-
-  function amountFor(aPerformance: any): number {
-    let result = 0
-    switch (playFor(aPerformance).type) {
-      case "tragedy":
-        result = 40000
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30)
-        }
-        break
-      case "comedy":
-        result = 30000
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20)
-        }
-        result += 300 * aPerformance.audience
-        break
-      default:
-        throw new Error(`Unknown type: ${playFor(aPerformance).type}.`)
-    }
-    return result
-  }
-
-  function playFor(aPerformance: any): any {
-    return plays[aPerformance.playID]
-  }
-
-  function volumeCreditsFor(aPerformance: any): number {
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0)
-    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5)
-    return result
-  }
 
   function formatAsUSD(aNumber: number): string {
     return new Intl.NumberFormat("en-US", {
@@ -53,22 +19,6 @@ function renderPlainText(data: any, plays: any) {
       currency: "USD",
       minimumFractionDigits: 2,
     }).format(aNumber / 100)
-  }
-
-  function totalVolumeCredits() {
-    let volumeCredits = 0
-    for (let perf of data.performances) {
-      volumeCredits += volumeCreditsFor(perf)
-    }
-    return volumeCredits
-  }
-
-  function totalAmount() {
-    let result = 0
-    for (let perf of data.performances) {
-      result += amountFor(perf)
-    }
-    return result
   }
 }
 export default statement
