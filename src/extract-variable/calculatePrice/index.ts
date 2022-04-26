df8812db9861115e81e416a87519897b5ddc6abd
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-04-26 08:26:28 +0400

    Ch. 2: Extract variable: Extract quantityDiscount var

--- src/extract-variable/calculatePrice/index.ts
+++ src/extract-variable/calculatePrice/index.ts
@@ -6,8 +6,9 @@ export type Order = {
 const calculatePrice = ({ anOrder }: { anOrder: Order }): number => {
   const { itemPrice, quantity } = anOrder
   const basePrice = quantity * itemPrice
+  const quantityDiscount = Math.max(0, quantity - 500) * itemPrice * 0.05
   // Price is: base price - quantity discount + shipping.
-  return basePrice - Math.max(0, quantity - 500) * itemPrice * 0.05 + Math.min(basePrice * 0.1, 100)
+  return basePrice - quantityDiscount + Math.min(basePrice * 0.1, 100)
 }
 
 export default calculatePrice
