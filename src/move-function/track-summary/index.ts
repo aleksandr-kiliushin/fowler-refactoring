8ab6a9395de028b5765b622696b5dca82db8c09c
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-05-21 18:31:12 +0400

    move-function/trackSummary pull all function to top level, inline function calls

--- src/move-function/track-summary/index.ts
+++ src/move-function/track-summary/index.ts
@@ -5,29 +5,19 @@ export type Result = {
 }
 
 const trackSummary = ({ points }: { points: unknown[] }): Result => {
-  const totalTime = calculateTotalTime()
-  const pace = totalTime / 60 / calculateTotalDistance({ points })
   return {
-    time: totalTime,
+    time: calculateTotalTime(),
     distance: calculateTotalDistance({ points }),
-    pace,
-  }
-
-  function calculateTotalTime(): number {
-    return 60000000
+    pace: calculateTotalTime() / 60 / calculateTotalDistance({ points }),
   }
 }
 
 const calculateTotalDistance = ({ points }: { points: unknown[] }): number => {
-  let result = 0
-  for (let i = 0; i < points.length; i++) {
-    result += calculateDistance({ pointA: points[i - 1], pointB: points[i] })
-  }
   return 1000
+}
 
-  function calculateDistance({ pointA, pointB }: { pointA: unknown; pointB: unknown }): number {
-    return 100
-  }
+const calculateTotalTime = (): number => {
+  return 60000000
 }
 
 export default trackSummary
